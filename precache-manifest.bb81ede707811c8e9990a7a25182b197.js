b7bff40549daef6ffee15a75857fd544763a9fb1
Josué Fuentes <alienxtrix@gmail.com>
2019-06-03 00:16:09 -0500

    Updates

---
+++ precache-manifest.bb81ede707811c8e9990a7a25182b197.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b6f393b8be03ab045625b4fe3823ebf",
+    "url": "/system/index.html"
+  },
+  {
+    "revision": "43a4ac206b0199ae1ed7",
+    "url": "/system/static/css/2.1dadb59b.chunk.css"
+  },
+  {
+    "revision": "1d7ca58d47abafbc43ba",
+    "url": "/system/static/css/main.53453d1f.chunk.css"
+  },
+  {
+    "revision": "43a4ac206b0199ae1ed7",
+    "url": "/system/static/js/2.c14f22c7.chunk.js"
+  },
+  {
+    "revision": "1d7ca58d47abafbc43ba",
+    "url": "/system/static/js/main.8eb78d59.chunk.js"
+  },
+  {
+    "revision": "14175cb434086629fee9",
+    "url": "/system/static/js/runtime~main.b3783146.js"
+  },
+  {
+    "revision": "71addf454967249472471f5398055e3d",
+    "url": "/system/static/media/astronauts.71addf45.svg"
+  },
+  {
+    "revision": "973f5842f0c37eacb2eb491c62fc0032",
+    "url": "/system/static/media/badge-header.973f5842.svg"
+  },
+  {
+    "revision": "b55cf24db0826b06a9a2ec8bf696c670",
+    "url": "/system/static/media/logocasaTransparencia.b55cf24d.png"
+  },
+  {
+    "revision": "1d17360a685ac86023b90aa14fd2c62d",
+    "url": "/system/static/media/stars.1d17360a.svg"
+  }
+]);
